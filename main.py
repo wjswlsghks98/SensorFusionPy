d868c8fcc974493cc34c0a77b72e452337546b0a
wjswlsghks98 <jinhwan0014@gmail.com>
2023-01-30 17:33:53 +0900

    Test

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from dataloader import DataLoader
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+
 """
 Main File for data processing and optimization
 Implemented by JinHwan Jeon, 2022
@@ -20,12 +22,18 @@ Implemented by JinHwan Jeon, 2022
 # 'D:/SJ_Dataset/2022-08-05/2022-08-05--03-13-16/'
 # 'D:/SJ_Dataset/2022-08-05/2022-08-05--03-45-16/'
 # 'D:/SJ_Dataset/2022-08-05/2022-08-05--04-19-33/'
+
+# Unindent from here
+
 file_path = 'D:/SJ_Dataset/2022-08-05/2022-08-05--02-41-03/'
 save_path = 'D:/SJ_Dataset/2022-08-05/2022-08-05--02-41-03/results/'
 
 # path = ''
 dataset = DataLoader(file_path,save_path)
 dataset.load()
+
+# to here
+
 
 """
 Check acceleration values to match axis correctly
@@ -55,6 +63,3 @@ For other dataset with multiple static scenes, check if time_bias terms are remo
 # plt.figure(5)
 # plt.plot(t,'k.')
 # plt.show()
-
-
-
